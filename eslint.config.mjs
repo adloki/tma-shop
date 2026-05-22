88914ffee9b0b4ede3edd0a071ab7099440d56d6
Adloki <helper_ts@mail.ru>
2026-05-22 11:46:14 +0300

    fix: remove __dirname for Edge compatibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,8 @@ import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
